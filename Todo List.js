3236690ca36333145ec3a10631970fa86e5424ea
luka1987 <zdravkovicx5b@gmail.com>
2017-02-16 23:03:53 +0100

    Add delete and changed list

--- Todo List.js
+++ Todo List.js
@@ -4,7 +4,11 @@ var input = prompt('What would you like to do?');
 
 while(input !== 'quit'){
 	if(input === 'list'){
-	console.log(todos);
+		console.log('*******************');
+		todos.forEach(function(todo, i){
+			console.log(i + ': ' +todo);
+		});
+		console.log('*******************');
 	}
 	else if (input === 'new'){
 	var newTodo = prompt('Enter new todo');
