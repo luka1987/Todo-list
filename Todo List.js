8da48ee02d87cd092c652e33ec976d09043827c6
luka1987 <zdravkovicx5b@gmail.com>
2017-02-17 21:39:40 +0100

    Refactoring code, added some functionality to code

--- Todo List.js
+++ Todo List.js
@@ -1,19 +1,37 @@
-var todos = ['Make big money'];
+var todos = [];
 
 var input = prompt('What would you like to do?');
 
 while(input !== 'quit'){
 	if(input === 'list'){
-		console.log('*******************');
-		todos.forEach(function(todo, i){
-			console.log(i + ': ' +todo);
-		});
-		console.log('*******************');
+		listTodos();
 	}
 	else if (input === 'new'){
-	var newTodo = prompt('Enter new todo');
-	todos.push(newTodo);
+		newTodo();
+	}
+	else if(input === 'delete'){
+		deleteTodo();	
 	}
 	input = prompt('What would you like to do?');
 }
 console.log('You quit the app!');
+
+function listTodos(){
+	console.log('*******************');
+		todos.forEach(function(todo, i){
+			console.log(i + ': ' +todo);
+		});
+	console.log('*******************');
+}
+
+function newTodo(){
+	var newTodo = prompt('Enter new todo');
+	todos.push(newTodo);
+	console.log('Todo has been added!');
+}
+
+function deleteTodo(){
+	var index = prompt("What index to delete?");
+	todos.splice(index, 1);
+	console.log("Todo has been deleted!");
+}
